afe0a994b62d2e50040f8ac9e8fb6f6e4830a3d2
ProGrad Junior <70092746+ProGradJr@users.noreply.github.com>
2020-11-08 12:48:09 +0530

    Add files via upload

---
+++ game.js
@@ -0,0 +1,21 @@
+
+
+var time = 20;
+var timerId;
+
+function startTimer() {
+    time = 20;
+    timer.innerHTML = time;
+    timerId = setInterval(() => {
+        time--;
+        if (time == 0) location.href = "./gameover.html?score=" + score;
+        timer.innerHTML = time;
+    }, 1000);
+}
+
+function resetTime(intervalId) {
+    clearInterval(intervalId);
+    startTimer();
+}
+
+startTimer();
